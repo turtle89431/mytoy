4e0e3c5c2e2c296b8bac1c2dafb2503f7e8bec76
turtle89431 <walterwinser@walters-MacBook.local>
2017-04-02 17:09:45 -0700

    Update

--- server.js
+++ server.js
@@ -3,14 +3,26 @@
  */
 var express = require('express')
 var app = express()
+//var dir='s'
+app.locals.dir = 's'
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(express.static('public'))
 app.get('/', function (req, res) {
     res.sendfile('public/index.html')
 })
-app.get('/update',function (req,res) {
-    res.send("Test is here")
+app.get('update',function (req,res) {
+    res.send(app.locals.dir)
 })
+app.route('/data').get(function (req,res) {
+    res.send(app.locals.dir)
+}).post(function (req,res) {
+    console.log(req.body.dir)
+    app.locals.dir = req.body.dir
 
+})
 app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!')
 })
